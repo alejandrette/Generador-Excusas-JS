766cdc9115cbc2f2e198665d6efafdd0d4f796f8
alejandro <120450320+alejandrette@users.noreply.github.com>
2024-08-21 10:08:14 +0000

    Generate Excuse

--- src/app.js
+++ src/app.js
@@ -9,3 +9,25 @@ window.onload = function() {
   //write your code here
   console.log("Hello Rigo from the console!");
 };
+
+let who = ["Mi perro", "Mi abuela", "Mi amigo", "Mi pajaro"];
+let action = ["se ha comido", "ha vomitado", "se ha cagado", "ha tirado"];
+let what = ["mi tarea", "mi movil", "mi coche", "mis zapatos"];
+let when = [
+  "después de clases",
+  "mientras dormia",
+  "cuando estaba cenando",
+  "en la piscina"
+];
+
+function getRandomElement(array) {
+  return array[Math.floor(Math.random() * array.length)];
+}
+
+function generateExcusa() {
+  return `${getRandomElement(who)} ${getRandomElement(
+    action
+  )} ${getRandomElement(what)} ${getRandomElement(when)}`;
+}
+
+document.getElementById("excusas").innerHTML = generateExcusa();
